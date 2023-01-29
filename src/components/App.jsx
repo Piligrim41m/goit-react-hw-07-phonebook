3d780368cb45b974cb3771a1a440a1d79e439880
Piligrim41m <lightsun41m@gmail.com>
2023-01-29 13:33:18 +0200

    add completed task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+
+import { Phonebook } from './phonebook/phonebook';
+import { Contacts } from './contacts/contacts';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Phonebook></Phonebook>
+      <Contacts></Contacts>
+    </>
   );
 };
